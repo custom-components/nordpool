8669caa0fb9e13e3e3e23c336905c47224a4b1e8
Hellowlol <Hellowlol@users.noreply.github.com>
2019-10-30 20:22:56 +0100

    ran black

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -35,17 +35,11 @@ _REGIONS = {
     "SE3": ["SEK", "Sweden", 0.25],
     "SE4": ["SEK", "Sweden", 0.25],
     # What zone is this?
-    "SYS": ["EUR", "System zone", 0.25]
-
+    "SYS": ["EUR", "System zone", 0.25],
 }
 
 # Needed incase a user wants the prices in non local currency
-_CURRENCY_TO_LOCAL = {
-    "DKK": "Kr",
-    "NOK": "Kr",
-    "SEK": "Kr",
-    "EUR": "€"
-}
+_CURRENCY_TO_LOCAL = {"DKK": "Kr", "NOK": "Kr", "SEK": "Kr", "EUR": "€"}
 
 DEFAULT_CURRENCY = "NOK"
 DEFAULT_REGION = "Kr.sand"
@@ -57,9 +51,9 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_REGION, default=DEFAULT_REGION): vol.In(
             list(_REGIONS.keys())
         ),
-        vol.Optional("friendly_name", default=''): cv.string,
+        vol.Optional("friendly_name", default=""): cv.string,
         # This is only needed if you want the some area but want the prices in a non local currency
-        vol.Optional("currency", default=''): cv.string,
+        vol.Optional("currency", default=""): cv.string,
         vol.Optional("VAT", default=True): vol.Boolean,
         vol.Optional("precision", default=3): cv.positive_int,
         vol.Optional("low_price_cutoff", default=1.0): cv.small_float,
@@ -78,10 +72,19 @@ def setup_platform(hass, config, add_devices, discovery_info=None) -> None:
     price_type = config.get("price_type")
     precision = config.get("precision")
     low_price_cutoff = config.get("low_price_cutoff")
-    currency = config.get('currency')
+    currency = config.get("currency")
     vat = config.get("VAT")
     api = hass.data[DOMAIN]
-    sensor = NordpoolSensor(friendly_name, region, price_type, precision, low_price_cutoff, currency, vat, api)
+    sensor = NordpoolSensor(
+        friendly_name,
+        region,
+        price_type,
+        precision,
+        low_price_cutoff,
+        currency,
+        vat,
+        api,
+    )
 
     add_devices([sensor])
 
@@ -94,18 +97,39 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     price_type = config.get("price_type")
     precision = config.get("precision")
     low_price_cutoff = config.get("low_price_cutoff")
-    currency = config.get('currency')
+    currency = config.get("currency")
     vat = config.get("VAT")
     api = hass.data[DOMAIN]
-    sensor = NordpoolSensor(friendly_name, region, price_type, precision, low_price_cutoff, currency, vat, api)
+    sensor = NordpoolSensor(
+        friendly_name,
+        region,
+        price_type,
+        precision,
+        low_price_cutoff,
+        currency,
+        vat,
+        api,
+    )
     async_add_devices([sensor])
 
 
 class NordpoolSensor(Entity):
     def __init__(
-        self, friendly_name, area, price_type, precision, low_price_cutoff, currency, vat, api
+        self,
+        friendly_name,
+        area,
+        price_type,
+        precision,
+        low_price_cutoff,
+        currency,
+        vat,
+        api,
     ) -> None:
-        self._friendly_name = friendly_name or '%s %s %s' % (DEFAULT_NAME, price_type, area)
+        self._friendly_name = friendly_name or "%s %s %s" % (
+            DEFAULT_NAME,
+            price_type,
+            area,
+        )
         self._area = area
         self._currency = currency or _REGIONS[area][0]
         self._price_type = price_type
@@ -156,14 +180,19 @@ class NordpoolSensor(Entity):
     @property
     def unit_of_measurement(self) -> str:
         """Return the unit of measurement this sensor expresses itself in."""
-        return '%s/%s' % (self._currency, self._price_type)
+        return "%s/%s" % (self._currency, self._price_type)
 
     @property
     def unique_id(self):
-        name = "nordpool_%s_%s_%s_%s_%s" % (self._price_type, self._area,
-                                            self._currency, self._precision,
-                                            self._low_price_cutoff, self._vat)
-        name = name.lower().replace('.', '')
+        name = "nordpool_%s_%s_%s_%s_%s" % (
+            self._price_type,
+            self._area,
+            self._currency,
+            self._precision,
+            self._low_price_cutoff,
+            self._vat,
+        )
+        name = name.lower().replace(".", "")
         return name
 
     @property
@@ -206,7 +235,7 @@ class NordpoolSensor(Entity):
 
         if has_junk(data):
             _LOGGER.info("It was junk infinty in api response, fixed it.")
-            d = extract_attrs(data.get('values'))
+            d = extract_attrs(data.get("values"))
             data.update(d)
 
         # we could check for peaks here.
@@ -232,10 +261,11 @@ class NordpoolSensor(Entity):
         tz = pendulum.now().timezone_name
         local_times = []
         for item in data.get("values", []):
-            i = {"start": pendulum.instance(item["start"]).in_timezone(tz),
-                 "end": pendulum.instance(item["end"]).in_timezone(tz),
-                 "value": item["value"]
-                 }
+            i = {
+                "start": pendulum.instance(item["start"]).in_timezone(tz),
+                "end": pendulum.instance(item["end"]).in_timezone(tz),
+                "value": item["value"],
+            }
 
             local_times.append(i)
 
@@ -283,7 +313,7 @@ class NordpoolSensor(Entity):
             "region": self._area,
             "low price": self.low_price,
             "today": self.today,
-            "tomorrow": self.tomorrow
+            "tomorrow": self.tomorrow,
         }
 
     def _update_current_price(self) -> None:
@@ -291,13 +321,13 @@ class NordpoolSensor(Entity):
         local_now = pendulum.now()
 
         if self._last_update_hourly is None or is_new(self._last_update_hourly, "hour"):
-            _LOGGER.debug("Updated _current_price!")
             data = self._api.today(self._area, self._currency)
             if data:
                 for item in self._someday(data):
                     if item["start"] == local_now.start_of("hour"):
                         self._current_price = item["value"]
                         self._last_update_hourly = local_now
+                        _LOGGER.debug("Updated _current_price %s", item["value"])
             else:
                 _LOGGER.info("Cant update _update_current_price because it was no data")
         else:
