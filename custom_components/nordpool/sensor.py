a057531b970efdcd85c381c114930227a1433fe6
priiduonu <mac@mtb.ee>
2020-02-26 23:23:07 +0200

    Corrected price unit W -> Wh

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -16,7 +16,7 @@ from .misc import is_new, has_junk, extract_attrs
 _LOGGER = logging.getLogger(__name__)
 
 
-_PRICE_IN = {"kWh": 1000, "MWh": 0, "W": 1000 * 1000}
+_PRICE_IN = {"kWh": 1000, "MWh": 0, "Wh": 1000 * 1000}
 _REGIONS = {
     "DK1": ["DKK", "Denmark", 0.25],
     "DK2": ["DKK", "Denmark", 0.25],
