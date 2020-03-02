e24e16707b37d35d9dcc4e41070306fcded2d3fe
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-03-02 19:24:41 +0100

    Add translation dict from currency to cent value

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -40,6 +40,7 @@ _REGIONS = {
 
 # Needed incase a user wants the prices in non local currency
 _CURRENCY_TO_LOCAL = {"DKK": "Kr", "NOK": "Kr", "SEK": "Kr", "EUR": "€"}
+_CURRENTY_TO_CENTS = {"DKK": "Øre", "NOK": "Øre", "SEK": "Öre", "EUR": "¢"}
 
 DEFAULT_CURRENCY = "NOK"
 DEFAULT_REGION = "Kr.sand"
