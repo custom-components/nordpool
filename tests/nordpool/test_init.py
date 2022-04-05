87c00a35c20bff3683d95985d48c0d803f8beada
Hellowlol <Hellowlol@users.noreply.github.com>
2022-04-05 22:34:03 +0200

    Update test_init.py

--- tests/nordpool/test_init.py
+++ tests/nordpool/test_init.py
@@ -38,39 +38,8 @@ async def test_setup(hass):
 
     assert DOMAIN in hass.data
 
-    api = hass.data[DOMAIN]
-    print(api)
-    # aioclient_mock.get(
-    #    "https://www.nordpoolgroup.com/api/marketdata/page/10?currency=NOK&endDate=27-03-2022",
-    #    json=load_fixture("raw_data_nok_summertime.json"),
-    # )
+    # api = hass.data[DOMAIN]
 
-    a = AioPrices(None, "NOK")
-    a.currency = "NOK"
-
-    def wrap():
-        data = defaultdict(dict)
-        parsed = a._parse_json(json.loads(load_fixture("raw_data_nok_summertime.json")))
-
-        data["NOK"]["today"] = join_result_for_correct_time([parsed], None)
-        return data
-
-    # print(wrap())
-
-    m = AsyncMock(return_value=wrap())
-    # Find the correct place to patch this..
-    with patch("custom_components.nordpool.aio_price.AioPrices.fetch", new=m(),), patch(
-        "custom_components.nordpool.sensor.AioPrices.fetch",
-        new=m(),
-    ), patch("custom_components.nordpool.AioPrices.fetch", new=m()):
-        # Real tests for now, lets patch or stub it later.
-        data_today_ok = await api.update_today(None)
-        print(api._data["today"])
-        assert data_today_ok is True
-
-    # data_tomorrow_ok = await api.update_tomorrow(None)
-    # assert data_today_ok is True
-
-    # now = dt_util.now()
+    now = dt_util.now()
     # async_fire_time_changed(hass, now + timedelta(hours=24))
     await hass.async_block_till_done()
