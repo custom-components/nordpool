f4c88ef1f9bfced4200481d193ca9c82a870f6d4
Hellowlol <Hellowlol@users.noreply.github.com>
2022-05-03 22:32:32 +0200

    bump to 0.0.6

--- custom_components/nordpool/__init__.py
+++ custom_components/nordpool/__init__.py
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 
 NAME = DOMAIN
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 ISSUEURL = "https://github.com/custom-components/nordpool/issues"
 
 STARTUP = f"""
