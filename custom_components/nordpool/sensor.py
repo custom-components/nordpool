843ee4096ab63ec116bcdbbe5efa7af315a3ae87
Jaap P <ajediiam@gmail.com>
2023-01-01 17:14:09 +0100

    Update sensor.py
    Tax went back from 9% to 21% because of... politics...

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -48,7 +48,7 @@ _REGIONS = {
     # What zone is this?
     "SYS": ["EUR", "System zone", 0.25],
     "FR": ["EUR", "France", 0.055],
-    "NL": ["EUR", "Netherlands", 0.09],
+    "NL": ["EUR", "Netherlands", 0.21],
     "BE": ["EUR", "Belgium", 0.21],
     "AT": ["EUR", "Austria", 0.20],
     # Tax is disabled for now, i need to split the areas
