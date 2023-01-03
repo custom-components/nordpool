434a81ffaca38668549fa7717b056ade00367fc3
Hellowlol <Hellowlol@users.noreply.github.com>
2023-01-03 21:51:16 +0100

    Debump

--- custom_components/nordpool/__init__.py
+++ custom_components/nordpool/__init__.py
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 
 NAME = DOMAIN
-VERSION = "0.0.11"
+VERSION = "0.0.10b2"
 ISSUEURL = "https://github.com/custom-components/nordpool/issues"
 
 STARTUP = f"""
