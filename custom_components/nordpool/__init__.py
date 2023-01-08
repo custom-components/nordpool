22c933d424b8ad356fa376013e9fe14d4b7835e8
Alexey Shlemenkov <nexemjail@gmail.com>
2023-01-08 14:34:52 +0000

    VAT reduction in FI

--- custom_components/nordpool/__init__.py
+++ custom_components/nordpool/__init__.py
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 
 NAME = DOMAIN
-VERSION = "0.0.10b3"
+VERSION = "0.0.10b4"
 ISSUEURL = "https://github.com/custom-components/nordpool/issues"
 
 STARTUP = f"""
