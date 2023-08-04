1b8c97ddcbd521a12f3f1fa79cd72dfa05c1798c
Francesco Frassinelli <fraph24@gmail.com>
2023-08-04 09:49:10 +0200

    Update VAT for Belgium

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -52,7 +52,7 @@ _REGIONS = {
     "SYS": ["EUR", "System zone", 0.25],
     "FR": ["EUR", "France", 0.055],
     "NL": ["EUR", "Netherlands", 0.21],
-    "BE": ["EUR", "Belgium", 0.21],
+    "BE": ["EUR", "Belgium", 0.06],
     "AT": ["EUR", "Austria", 0.20],
     # Tax is disabled for now, i need to split the areas
     # to handle the tax.
