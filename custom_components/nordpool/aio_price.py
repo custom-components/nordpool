custom-components/nordpool

commit 56379f3c156ee430338ec330c9ebc45c260f2669

Raise exception if data is not available when update is called
This happens if the first auction is unsuccessful.

Implemented by verifying that the data is valid (in
join_result_for_correct_time()) and raising an exception if it isn't.

This partially fixes issue #326.

custom_components/nordpool/aio_price.py
@@ -90,6 +90,12 @@ AREA_TO_COUNTRY = {
     "PL ": "PL",
 }
 
+INVALID_VALUES = frozenset((None, float("inf")))
+
+
+class InvalidValueException(ValueError):
+    pass
+
 
 def join_result_for_correct_time(results, dt):
     """Parse a list of responses from the api
@@ -135,6 +141,8 @@ def join_result_for_correct_time(results, dt):
                 local = val["start"].astimezone(zone)
                 local_end = val["end"].astimezone(zone)
                 if start_of_day <= local and local <= end_of_day:
+                    if val['value'] in INVALID_VALUES:
+                        raise InvalidValueException()
                     if local == local_end:
                         _LOGGER.info(
                             "Hour has the same start and end, most likly due to dst change %s exluded this hour",

commit 7209e05c87673a5577e96956509f483955729612

Reverse order of operations to properly ignore DST issues in data
Bug introduced by yours truly in commit 56379f3c156e.

custom_components/nordpool/aio_price.py
@@ -141,13 +141,13 @@ def join_result_for_correct_time(results, dt):
                 local = val["start"].astimezone(zone)
                 local_end = val["end"].astimezone(zone)
                 if start_of_day <= local and local <= end_of_day:
-                    if val['value'] in INVALID_VALUES:
-                        raise InvalidValueException(f"Invalid value in {val} for area '{key}'")
                     if local == local_end:
                         _LOGGER.info(
                             "Hour has the same start and end, most likly due to dst change %s exluded this hour",
                             val,
                         )
+                    elif val['value'] in INVALID_VALUES:
+                        raise InvalidValueException(f"Invalid value in {val} for area '{key}'")
                     else:
                         fin["areas"][key]["values"].append(val)
 