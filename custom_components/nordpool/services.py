0720c18a26d494b8251ed8cb7ff27814473f7f4d
Steffen Fredriksen <Hellowlol@users.noreply.github.com>
2024-12-29 00:55:28 +0100

    Second

--- custom_components/nordpool/services.py
+++ custom_components/nordpool/services.py
@@ -1,28 +1,92 @@
 import logging
 
+from typing import Any
+
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, ServiceCall
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
+from .const import _REGIONS
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-DO_ACITON_SCHEMA = vol.Schema(
-    {vol.Optional("currency"): str, vol.Optional("something"): str}
+HOURLY_SCHEMA = vol.Schema(
+    {
+        vol.Required("currency"): str,
+        vol.Required("date"): cv.date,
+        vol.Required("area"): vol.In(list(_REGIONS.keys())),
+    }
+)
+
+
+YEAR_SCHEMA = vol.Schema(
+    {
+        vol.Required("currency"): str,
+        vol.Required("year"): cv.matches_regex(r"^[1|2]\d{3}$"),
+        vol.Required("area"): vol.ensure_list,
+    }
 )
 
 
 async def async_setup_services(hass: HomeAssistant):
     _LOGGER.debug("Setting up services")
-    hass.services.async_register("NORDPOOL", DO_ACITON_SCHEMA, do_action)
+    from .aio_price import AioPrices
 
+    client = async_get_clientsession(hass)
 
-async def do_action(hass: HomeAssistant, service_call: ServiceCall):
-    _LOGGER.debug("called do_action with %s" % service_call)
-    api = hass.data.get("NORDPOOL")
-    if api:
-        data = await api.monthly()
-        print(data)
+    async def hourly(service_call: ServiceCall) -> Any:
+        sc = service_call.data
+
+        return await AioPrices(sc["currency"], client).hourly(
+            areas=sc["area"], end_date=sc["date"]
+        )
+
+    async def yearly(service_call: ServiceCall):
+        sc = service_call.data
+        return await AioPrices(sc["currency"], client).yearly(
+            areas=sc["area"], end_date=sc["year"]
+        )
+
+    async def monthly(service_call: ServiceCall):
+        sc = service_call.data
+        return await AioPrices(sc["currency"], client).monthly(
+            areas=sc["area"], end_date=sc["year"]
+        )
+
+    async def daily(service_call: ServiceCall):
+        sc = service_call.data
+        return await AioPrices(sc["currency"], client).daily(
+            areas=sc["area"], end_date=sc["year"]
+        )
+
+    hass.services.async_register(
+        domain="nordpool",
+        service="hourly",
+        service_func=hourly,
+        schema=HOURLY_SCHEMA,
+        supports_response=SupportsResponse.OPTIONAL,
+    )
+    hass.services.async_register(
+        domain="nordpool",
+        service="yearly",
+        service_func=yearly,
+        schema=YEAR_SCHEMA,
+        supports_response=SupportsResponse.OPTIONAL,
+    )
+    hass.services.async_register(
+        domain="nordpool",
+        service="monthly",
+        service_func=monthly,
+        schema=YEAR_SCHEMA,
+        supports_response=SupportsResponse.OPTIONAL,
+    )
+    hass.services.async_register(
+        domain="nordpool",
+        service="daily",
+        service_func=daily,
+        schema=YEAR_SCHEMA,
+        supports_response=SupportsResponse.OPTIONAL,
+    )
