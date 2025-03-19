fa17557963dd31bcc1cd7d3df457290ea4d5f8b0
Iskra Delta <iskradelta@users.noreply.github.com>
2025-03-19 16:46:55 +0000

    negative prices vat should do correct calculation

--- custom_components/nordpool/sensor.py
+++ custom_components/nordpool/sensor.py
@@ -261,7 +261,7 @@ class NordpoolSensor(SensorEntity):
 
             return pass_context(inner)
 
-        price = value / _PRICE_IN[self._price_type] * (float(1 + self._vat))
+        price = value / _PRICE_IN[self._price_type] + (_PRICE_IN[self._price_type]*float(self._vat))
         template_value = self._ad_template.async_render(
             now=faker(), current_price=price
         )
